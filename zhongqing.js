8c76995876ae913fe4da7058ade7c56b1a629b1f
FLychicken666 <63433790+FLychicken666@users.noreply.github.com>
2020-11-20 16:38:51 +0800

    Update zhongqing.js

--- zhongqing.js
+++ zhongqing.js
@@ -1,5 +1,4 @@
-hostname = *.youth.cn
-//改造了一下中青自动阅读，支持qx,loon等。
-//自动获取body。 稍后会更新boxJs。支持账号切换。欢迎测试
-//获取body
-http-request https:\/\/ios\.baertt\.com\/v5\/article\/complete script-path=https://cdn.jsdelivr.net/gh/songyangzz/QuantumultX@master/zqgetbody.js, requires-body=true, timeout=100, tag=获取body
+http-request https://ios.baertt.com/v5/article/complete.json script-path=https://raw.githubusercontent.com/songyangzz/QuantumultX/master/zqgetbody.js, requires-body=true, timeout=10, tag=中青看点获取body
+
+
+hostname =  ios.baertt.com
